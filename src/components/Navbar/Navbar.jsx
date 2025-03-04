e1d364f236a8565a32901285f63e5a0c8c9c5d7b
Johana Castellano <johanacg16@gmail.com>
2025-03-04 14:33:54 +0000

    closeSideBar function implemented in navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { AuthContext } from "../../context/auth.context";
 
 function Navbar() {
@@ -8,9 +8,19 @@ function Navbar() {
   // the values from AuthContext.Provider's `value` prop
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
+  // Se crea un ref para controlar el checkbox
+  const drawerCheckbox = useRef(null);
+
+  const closeSidebar = () => {
+    // Cambiar el valor del checkbox a false para cerrar el sidebar
+    if (drawerCheckbox.current) {
+      drawerCheckbox.current.checked = false;
+    }
+  };
+
   return (
     <nav className="drawer">
-      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" ref={drawerCheckbox} /* Se asigna el ref al checkbox */ />
       <div className="drawer-content flex flex-col">
         {/* Navbar */}
         <div className="navbar bg-base-300 w-full">
@@ -64,9 +74,25 @@ function Navbar() {
         <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
         <ul className="menu bg-base-200 min-h-full w-80 p-4">
           {/* Sidebar content here */}
-          <li><Link to="/">Municipios</Link></li>
-          <li><Link to="/">Mercados</Link></li>
-          <li><Link to="/">Consume Local</Link></li>
+          <button 
+          className="btn btn-circle btn-outline btn-sm self-end mb-2"
+          onClick={closeSidebar}> {/* Para cerrar el sidebar al hacer clic en este botón */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+          <li><Link to="/" onClick={closeSidebar}>Municipios</Link></li>
+          <li><Link to="/" onClick={closeSidebar}>Mercados</Link></li>
+          <li><Link to="/" onClick={closeSidebar}>Consume Local</Link></li>
           {isLoggedIn && (
             <>
               <li><button onClick={logOutUser}>Cerrar Sesión</button></li>
@@ -81,8 +107,8 @@ function Navbar() {
           )}
           {!isLoggedIn && (
             <>
-              <li><Link to="/signup" className="btn my-4">Registrarse</Link></li>
-              <li><Link to="/login" className="btn">Acceder</Link></li>
+              <li><Link to="/signup" onClick={closeSidebar} className="btn my-4">Registrarse</Link></li>
+              <li><Link to="/login" onClick={closeSidebar} className="btn">Acceder</Link></li>
             </>
           )}
         </ul>
